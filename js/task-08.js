b0de129b020789bbe62788100be80b01f54927e0
Kateryna-Borysenko <k.borysenko.kyiv@gmail.com>
2021-11-09 20:42:17 +0200

    Second part of tasks

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const { login, password } = event.currentTarget.elements;
+    let flag = false;
+    const formData = new FormData(form);
+    const data = {};
+    formData.forEach((value, key) => {
+        data[key] = value;
+        if (!value) {
+            flag = true;
+        }
+    });
+    if (flag) {
+        return alert('Bсе поля должны быть заполнены!');
+    }
+    console.log(data);
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  textColor: document.querySelector(".color"),
+  btn: document.querySelector("button"),
+  body: document.querySelector("body"),
+};
+
+refs.btn.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  refs.body.style.backgroundColor = color;
+  refs.textColor.textContent = color;
+});
