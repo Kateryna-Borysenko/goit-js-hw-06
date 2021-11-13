f98962885566944335de72d1abf26f3562256588
Kateryna-Borysenko <k.borysenko.kyiv@gmail.com>
2021-11-13 20:50:43 +0200

    Added second way to solve task

--- js/task-08.js
+++ js/task-08.js
@@ -1,20 +1,32 @@
 const form = document.querySelector(".login-form");
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const { login, password } = event.currentTarget.elements;
-    let flag = false;
-    const formData = new FormData(form);
-    const data = {};
-    formData.forEach((value, key) => {
-        data[key] = value;
-        if (!value) {
-            flag = true;
-        }
-    });
-    if (flag) {
-        return alert('Bсе поля должны быть заполнены!');
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     const { login, password } = event.currentTarget.elements;
+//     let flag = false;
+//     const formData = new FormData(form);
+//     const data = {};
+//     formData.forEach((value, key) => {
+//         data[key] = value;
+//         if (!value) {
+//             flag = true;
+//         }
+//     });
+//     if (flag) {
+//         return alert('Bсе поля должны быть заполнены!');
+//     }
+//     console.log(data);
+//     form.reset();
+// });
+
+//Второй способ ришения задачи
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const { email, password } = e.currentTarget.elements;
+    console.log(email, password);
+    if (email.value === "" || password.value === "") {
+        return alert('Bсе поля должны быть заполнены!')
     }
-    console.log(data);
-    form.reset();
+    const formData = { email: email.value, password: password.value }
+    console.log(formData);
 });
